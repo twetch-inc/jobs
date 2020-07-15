119efded56d3237266ba9fb61eee63a89368ce84
jdh7190 <48966141+jdh7190@users.noreply.github.com>
2020-07-15 16:12:37 -0400

    Add files via upload

---
+++ serviceworker.js
@@ -0,0 +1,30 @@
+var CACHE_NAME = 'v0.01'
+var urlsToCache = [
+  '/index.html'
+]
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Caching...')
+        cache.addAll(urlsToCache)
+      }).then(() => {
+        self.skipWaiting();
+      })
+  )
+})
+self.addEventListener('activate', (event) => {
+  console.log('activated')
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      )
+    })
+  )
+})
+self.addEventListener('fetch', e => {e.respondWith(fetch(e.request).catch(() => caches.match(e.request)))})
